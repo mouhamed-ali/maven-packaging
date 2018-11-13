7bbd9224592de22c19b92d92700d7b7138c9e04f
Amdouni Mohamed Ali <amdouni.mouhamed.ali@gmail.com>
2018-11-13 17:02:51 +0100

    [feat] - change the common configuration file directory

--- spring-profile-packaging-example/src/main/java/org/maven/tutorial/examples/controllers/ShowAppProprties.java
+++ spring-profile-packaging-example/src/main/java/org/maven/tutorial/examples/controllers/ShowAppProprties.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@PropertySource("classpath:description.properties")
+@PropertySource("classpath:common/description.properties")
 public class ShowAppProprties {
 
     @Autowired
