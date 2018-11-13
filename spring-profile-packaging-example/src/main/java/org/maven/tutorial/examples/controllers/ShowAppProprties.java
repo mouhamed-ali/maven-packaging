2b37043c5da842a6dde2a2b14c7c1c4a45ccb252
Amdouni Mohamed Ali <amdouni.mouhamed.ali@gmail.com>
2018-11-13 17:01:08 +0100

    [feat] - add the common configuration file

--- spring-profile-packaging-example/src/main/java/org/maven/tutorial/examples/controllers/ShowAppProprties.java
+++ spring-profile-packaging-example/src/main/java/org/maven/tutorial/examples/controllers/ShowAppProprties.java
@@ -2,18 +2,30 @@ package org.maven.tutorial.examples.controllers;
 
 import org.maven.tutorial.examples.config.ApplicationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource("classpath:description.properties")
 public class ShowAppProprties {
 
     @Autowired
-    ApplicationProperties applicationProperties;
+    private ApplicationProperties applicationProperties;
+    @Autowired
+    private Environment env;
+
 
     @GetMapping
     public String showProperty() {
 
         return applicationProperties.getPlatform();
+    }
+
+    @GetMapping("/description")
+    public String showDescription() {
+
+        return env.getProperty("description");
     }
 }
